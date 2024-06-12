51e442a595943dc1c3b9247b8c42451b84256bfc
Martin Rojas <csamuelr.lp@gmail.com>
2024-06-12 20:46:15 -0300

    Configuracion vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: vercel(),
+});
